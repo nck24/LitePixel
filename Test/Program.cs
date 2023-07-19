c86a628d72d49fce7fdac38b89d478d89e71fe0c
nck24 <nikocabello04@gmail.com>
2023-07-19 20:29:30 +0200

    The test folder is set up

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LitePixel;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Window w = new Window();
+
+            Console.ReadLine();
+        }
+    }
+}
